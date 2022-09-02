0396ac51ce0d8fb44736115f16dc8190418a48be
abdhulla-k <abdhullak149@gmail.com>
2022-09-02 13:54:05 +0530

    completed email and password verification

--- routes/home.js
+++ routes/home.js
@@ -13,10 +13,26 @@ const data = [
     { url : "https://media.istockphoto.com/photos/taj-mahal-sunrise-picture-id155096944?k=20&m=155096944&s=612x612&w=0&h=Pt-Li__LwwiTJtk8Cu5oNVL8jwUtlgWK6CKLbByXjiQ=", content: "Tajmahal. A mornimant in India" },
 ];
 
+const authData = { email: 'testuser@gmail.com', password: 'abcdefgh' };
+let authStatus = { emailStatus: '', passwordStatus: '' };
+
 router.post( '/', ( req, res, next ) => {
-    let userData = { username: req.body.username, password: req.body.password };
-    console.log( req.body );
-    res.render( 'home', { pageTitle: 'Home', products: data } );
+
+    // save auth data
+    let userData = { email: req.body.email, password: req.body.password };
+
+
+    authStatus.emailStatus = userData.email === 'testuser@gmail.com' ? '' : 'invalid emial';
+    authStatus.passwordStatus = userData.password === "abcdefgh" ? '' : 'invalid password';
+
+    // check is it correct or not
+    if( userData.email === authData.email && userData.password === authData.password ) {
+        res.render( 'home', { pageTitle: 'Home', products: data } );
+
+    } else {
+        res.redirect( '/login' );
+    }
 });
 
 exports.routes = router;
+exports.authStatus = authStatus;
--- routes/login.js
+++ routes/login.js
@@ -2,8 +2,14 @@ const express = require( 'express' );
 
 const router = express.Router();
 
+// get data status
+let homeData = require( "./home" );
+
 router.use( '/login', ( req, res, next ) => {
-    res.render( 'login' );
+    res.render( 'login', {
+        emailStatus: homeData.authStatus.emailStatus,
+        passwordStatus: homeData.authStatus.passwordStatus
+    });
 });
 
 exports.routes = router;
