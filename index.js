49be21fe40e03196339cf9289ff152db86347bc5
abdhulla-k <abdhullak149@gmail.com>
2022-09-02 09:51:27 +0530

    created login page

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require( 'express' );
+
+const path = require( 'path' );
+
+const bodyParser = require( 'body-parser' );
+
+// import routes
+const loginRoute = require( './routes/login' );
+
+const app = express();
+
+// use body parser
+app.use( bodyParser.urlencoded( { extended: false } ));
+
+// set view engine
+app.set( 'view engine', 'pug' );
+app.set( 'views', 'views' );
+
+app.use( express.static( path.join( __dirname, 'public' )))
+
+app.use( loginRoute.routes );
+
+// able to listen the application
+app.listen( 5000 );
