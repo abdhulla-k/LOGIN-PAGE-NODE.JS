e6428daa123db2b12ab8dd2afc5006387c4357f8
abdhulla-k <abdhullak149@gmail.com>
2022-09-02 12:36:02 +0530

    solved an error in form. now possible to access form data

--- routes/home.js
+++ routes/home.js
@@ -13,7 +13,7 @@ const data = [
     { url : "https://media.istockphoto.com/photos/taj-mahal-sunrise-picture-id155096944?k=20&m=155096944&s=612x612&w=0&h=Pt-Li__LwwiTJtk8Cu5oNVL8jwUtlgWK6CKLbByXjiQ=", content: "Tajmahal. A mornimant in India" },
 ];
 
-router.use( '/', ( req, res, next ) => {
+router.post( '/', ( req, res, next ) => {
     let userData = { username: req.body.username, password: req.body.password };
     console.log( req.body );
     res.render( 'home', { pageTitle: 'Home', products: data } );
--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,7 @@ const express = require( 'express' );
 
 const router = express.Router();
 
-router.get( '/login', ( req, res, next ) => {
+router.use( '/login', ( req, res, next ) => {
     res.render( 'login' );
 });
 
